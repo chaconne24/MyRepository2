0c0c3681cc6c9651f8a76bc78880fcfef2b9fe87
chaconne24 <417609244@qq.com>
2021-09-16 19:15:22 +0800

    ssh提交

--- StudentManagementSystemV1.0/src/com/xzsj/Resource/IResourceDao.java
+++
@@ -1,7 +0,0 @@
-package com.xzsj.Resource;
-
-import java.util.ArrayList;
-
-public interface IResourceDao {
-	public abstract ArrayList<Resource> getResourceList();
-}
--- StudentManagementSystemV1.0/src/com/xzsj/users/IUserDao.java
+++
@@ -1,5 +0,0 @@
-package com.xzsj.users;
-
-public interface IUserDao {
-	public abstract User vaildataLogin(User user);
-}
